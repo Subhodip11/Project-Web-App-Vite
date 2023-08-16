0b721a18c3f75b9d77836ad236048c0db75038a1
Subhodip11 <1941012209.subhodip@gmail.com>
2023-08-16 21:34:01 +0530

    completed social media backend api

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ app.use(express.json())
 
 const {conn} = require('./dbconfig.js');
 
-app.use('/api', require('./routes/userRoutes'), require('./routes/postRoutes'))
 
 const port = process.env.PORT || 8001
 
-
 conn.then(db=>{
     if(!db) return "Error connecting db"
+    app.use('/api', require('./routes/userRoutes'), require('./routes/postRoutes'))
     console.log("Database connected...")
     app.listen(port, ()=>{
         console.log(`Server started at port - ${port}`);
